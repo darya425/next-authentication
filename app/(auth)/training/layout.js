7a250b16ec69fa0b6f3f6dbd15c16a9b75d9c8d1
Darya Kuliashova <darya.kuliashova@generationimpact.global>
2025-07-30 11:09:50 +0200

    Add authentication app

---
+++ app/(auth)/training/layout.js
@@ -0,0 +1,15 @@
+import { logout } from '@/actions/auth-actions';
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <header id="auth-header">
+        <p>Welcome back!</p>
+        <form action={logout}>
+          <button>Logout</button>
+        </form>
+      </header>
+      {children}
+    </>
+  );
+}
